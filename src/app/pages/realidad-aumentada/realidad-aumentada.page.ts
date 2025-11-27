61f613fd61ca96b74629a50bc369372699dbe231
camilodangaud <143123036+camilodangaud@users.noreply.github.com>
2025-11-27 17:33:30 -0500

    realidad aumentada

--- src/app/pages/realidad-aumentada/realidad-aumentada.page.ts
+++ src/app/pages/realidad-aumentada/realidad-aumentada.page.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { ArConfigService } from '../../services/RA-config.service';
 
 @Component({
   selector: 'app-realidad-aumentada',
   templateUrl: './realidad-aumentada.page.html',
   styleUrls: ['./realidad-aumentada.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class RealidadAumentadaPage implements OnInit {
 
-  constructor() { }
+  arSceneUrl: SafeResourceUrl | null = null;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private arConfig: ArConfigService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-  }
+    const target = this.arConfig.getCurrentTarget();
 
+    if (!target) {
+      this.router.navigate(['/home']);
+      return;
+    }
+
+    // Devuelve: marker-carro, marker-robot, etc.
+    const markerId = this.arConfig.getMarkerIdFor(target);
+
+    // Archivo que contiene todos los marcadores dinámicos
+    const url = `assets/utiles/marker-scene.html?marker=${markerId}`;
+
+    this.arSceneUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+
+    console.log("Cargando escena AR con marker:", markerId);
+  }
 }
